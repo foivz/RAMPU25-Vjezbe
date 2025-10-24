8790c1cb9430fa5b28cdc8b3c7dd5ad740623ce4
Dario Ljubas <darioljubas33@gmail.com>
2025-10-24 11:44:21 +0200

    Integrate task list into PendingFragment

--- app/src/main/java/com/example/memento/fragments/PendingFragment.kt
+++ app/src/main/java/com/example/memento/fragments/PendingFragment.kt
@@ -5,12 +5,25 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.memento.MockDataLoader
 import com.example.memento.R
+import com.example.memento.adapters.TasksAdapter
+
 class PendingFragment : Fragment() {
+    private lateinit var recyclerView: RecyclerView
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_pending, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        recyclerView = view.findViewById(R.id.rv_pending_tasks)
+        recyclerView.adapter = TasksAdapter(MockDataLoader.getDemoData())
+        recyclerView.layoutManager = LinearLayoutManager(view.context)
     }
 }
