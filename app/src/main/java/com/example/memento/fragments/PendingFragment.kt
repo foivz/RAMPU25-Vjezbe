6d3dcaf1fa0169310f17a9471019f7611b34297e
Dario Ljubas <darioljubas33@gmail.com>
2025-11-04 15:52:58 +0100

    Implement new task creation dialog logic

--- app/src/main/java/com/example/memento/fragments/PendingFragment.kt
+++ app/src/main/java/com/example/memento/fragments/PendingFragment.kt
@@ -1,5 +1,6 @@
 package com.example.memento.fragments
 
+import android.app.AlertDialog
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -25,8 +26,49 @@ class PendingFragment : Fragment() {
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        fab = view.findViewById(R.id.fab_pending_fragment_create_task)
         recyclerView = view.findViewById(R.id.rv_pending_tasks)
         recyclerView.adapter = TasksAdapter(MockDataLoader.getDemoData())
         recyclerView.layoutManager = LinearLayoutManager(view.context)
+
+        fab.setOnClickListener {
+            showDialog()
+        }
+
+
+
+
+    }
+
+    private fun showDialog() {
+        val newTaskDialogView = LayoutInflater
+            .from(context)
+            .inflate(R.layout.new_task_dialog, null)
+
+        val dialogHelper = NewTaskDialogHelper(newTaskDialogView)
+        dialogHelper.populateSpinner(
+            MockDataLoader.getDemoCategories()
+        )
+        dialogHelper.activateDateTimeListeners()
+
+        AlertDialog.Builder(context)
+            .setView(newTaskDialogView)
+            .setTitle(getString(R.string.create_a_new_task))
+            .setPositiveButton(getString(R.string.create_a_new_task)) { _, _ ->
+                val newTask = dialogHelper.buildTask()
+                val tasksAdapter = (recyclerView.adapter as TasksAdapter)
+                tasksAdapter.addTask(newTask)
+            }
+            .show()
     }
 }
+
+
+
+
+
+
+
+
+
+
---
+++ app/src/main/java/com/example/memento/helpers/NewTaskDialogHelper.kt
@@ -0,0 +1,113 @@
+package com.example.memento.helpers
+
+import android.app.DatePickerDialog
+import android.app.TimePickerDialog
+import android.view.View
+import android.widget.ArrayAdapter
+import android.widget.DatePicker
+import android.widget.EditText
+import android.widget.Spinner
+import android.widget.TimePicker
+import com.example.memento.R
+import com.example.memento.entities.Task
+import com.example.memento.entities.TaskCategory
+import java.text.SimpleDateFormat
+import java.util.Calendar
+import java.util.Locale
+
+class NewTaskDialogHelper(private val view: View) {
+    private val selectedDateTime: Calendar = Calendar.getInstance()
+
+    private val sdfDate = SimpleDateFormat("dd.MM.yyyy.", Locale.ENGLISH)
+    private val sdfTime = SimpleDateFormat("HH:mm", Locale.ENGLISH)
+
+    private val spinner = view.findViewById<Spinner>(R.id.spn_new_task_dialog_category)
+    private val dateSelection = view.findViewById<EditText>(R.id.et_new_task_dialog_date)
+    private val timeSelection = view.findViewById<EditText>(R.id.et_new_task_dialog_time)
+
+    fun populateSpinner(categories: List<TaskCategory>) {
+        val spinnerAdapter = ArrayAdapter(
+            view.context,
+            android.R.layout.simple_spinner_item,
+            categories
+        )
+        spinnerAdapter.setDropDownViewResource(
+            android.R.layout.simple_spinner_dropdown_item
+        )
+        spinner.adapter = spinnerAdapter
+    }
+
+    fun activateDateTimeListeners() {
+        dateSelection.onFocusChangeListener = object : View.OnFocusChangeListener {
+            override fun onFocusChange(view: View, hasFocus: Boolean) {
+                if (hasFocus) {
+                    DatePickerDialog(
+                        view.context,
+                        object : DatePickerDialog.OnDateSetListener {
+                            override fun onDateSet(
+                                d: DatePicker?,
+                                year: Int,
+                                monthOfYear: Int,
+                                dayOfMonth: Int
+                            ) {
+                                selectedDateTime.set(year, monthOfYear, dayOfMonth)
+                                dateSelection.setText(sdfDate.format(selectedDateTime.time).toString())
+                            }
+
+                        },
+                        selectedDateTime.get(Calendar.YEAR),
+                        selectedDateTime.get(Calendar.MONTH),
+                        selectedDateTime.get(Calendar.DAY_OF_MONTH)
+                    ).show()
+                    view.clearFocus()
+                }
+            }
+        }
+
+        timeSelection.onFocusChangeListener = object : View.OnFocusChangeListener {
+            override fun onFocusChange(view: View, hasFocus: Boolean) {
+                if (hasFocus) {
+                    TimePickerDialog(
+                        view!!.context,
+                        object : TimePickerDialog.OnTimeSetListener {
+                            override fun onTimeSet(
+                                t: TimePicker?,
+                                hourOfDay: Int,
+                                minute: Int
+                            ) {
+                                selectedDateTime.set(Calendar.HOUR_OF_DAY, hourOfDay)
+                                selectedDateTime.set(Calendar.MINUTE, minute)
+                                timeSelection.setText(sdfTime.format(selectedDateTime.time).toString())
+                            }
+                        },
+                        selectedDateTime.get(Calendar.HOUR_OF_DAY),
+                        selectedDateTime.get(Calendar.MINUTE),
+                        true
+                    ).show()
+                    view.clearFocus()
+                }
+            }
+        }
+    }
+
+    fun buildTask(): Task {
+        val etName = view.findViewById<EditText>(R.id.et_new_task_dialog_name)
+        val newTaskName = etName.text.toString()
+        val spinnerCategory = view.findViewById<Spinner>(R.id.spn_new_task_dialog_category)
+        val selectedCategory = spinnerCategory.selectedItem as TaskCategory
+
+        return Task(
+            newTaskName,
+            selectedDateTime.time,
+            selectedCategory,
+            false
+        )
+    }
+
+
+}
+
+
+
+
+
