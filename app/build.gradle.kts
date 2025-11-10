637ae85ae188ffef58749024918f75fc55bb9416
Dario Ljubas <darioljubas33@gmail.com>
2025-11-10 15:47:57 +0100

    Add dependencies for working with Room

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.ksp)
 }
 
 android {
@@ -37,6 +38,9 @@ android {
 
 dependencies {
 
+
+    implementation(libs.androidx.room.runtime)
+    ksp("androidx.room:room-compiler:2.8.3")
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
