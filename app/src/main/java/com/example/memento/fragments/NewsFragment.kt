a4f36056d9edbe43e44cd09aba4b759155d1a5dd
Dario Ljubas <darioljubas33@gmail.com>
2025-11-17 16:34:05 +0100

    Implement news fetching and display logic in NewsFragment

--- app/src/main/java/com/example/memento/fragments/NewsFragment.kt
+++ app/src/main/java/com/example/memento/fragments/NewsFragment.kt
@@ -5,13 +5,91 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ProgressBar
+import android.widget.Toast
+import androidx.core.view.isVisible
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.memento.R
+import com.example.memento.adapters.NewsAdapter
+import com.example.memento.ws.NewsResponse
+import com.example.memento.ws.WsNews
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class NewsFragment : Fragment() {
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var loadingCircle: ProgressBar
+    private lateinit var btnRefresh: FloatingActionButton
+    private val ws = WsNews.newsService
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_news, container, false)
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        loadingCircle = view.findViewById(R.id.pb_news_fragment_loading)
+        btnRefresh = view.findViewById(R.id.fab_news_fragment_refresh)
+        recyclerView = view.findViewById(R.id.rv_news)
+
+        btnRefresh.setOnClickListener {
+            loadNews()
+        }
+
+        recyclerView.layoutManager = LinearLayoutManager(context)
+        loadNews()
+    }
+
+    private fun loadNews() {
+        changeDisplay(true)
+        ws.getNews().enqueue(object : Callback<NewsResponse> {
+            override fun onResponse(
+                call: Call<NewsResponse?>,
+                response: Response<NewsResponse?>
+            ) {
+                if (response.isSuccessful) {
+                    val responseBody = response.body()
+                    val news = responseBody?.results
+                    if (news != null) {
+                        recyclerView.adapter = NewsAdapter(news)
+                    }
+                } else {
+                    displayWebServiceErrorMessage()
+                }
+
+                changeDisplay(false)
+            }
+
+            override fun onFailure(
+                call: Call<NewsResponse?>,
+                t: Throwable
+            ) {
+                displayWebServiceErrorMessage()
+                changeDisplay(false)
+            }
+
+        })
+    }
+
+    private fun changeDisplay(isLoading: Boolean) {
+        btnRefresh.isVisible = !isLoading
+        recyclerView.isVisible = !isLoading
+        loadingCircle.isVisible = isLoading
+    }
+
+    private fun displayWebServiceErrorMessage() {
+        Toast.makeText(
+            context,
+            getString(R.string.news_ws_err_msg),
+            Toast.LENGTH_LONG
+        ).show()
+    }
 }
+
+
+
